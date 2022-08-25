535f616068f0cd141a75d5e2cc87492eba0b1c75
Lakshmana chimata <lakshmana@Lakshmanas-MacBook-Pro.local>
2022-08-25 14:53:40 +0530

    added mac stuff

---
+++ nearhop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nearhop
+//
+//  Created by Lakshmana chimata on 25/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nearhop/nearhopApp.swift
@@ -0,0 +1,17 @@
+//
+//  nearhopApp.swift
+//  nearhop
+//
+//  Created by Lakshmana chimata on 25/08/22.
+//
+
+import SwiftUI
+
+@main
+struct nearhopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
